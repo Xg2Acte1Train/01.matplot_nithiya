ef45d13ad77af511c846b17342a01478d9cc56cb
shrinivasan <shrinivasan@netcalyx.co>
2021-07-19 17:06:48 +0530

    second

--- 01_plots.py
+++ 01_plots.py
@@ -6,21 +6,31 @@ plt.plot(y)
 plt.show()
 
 x = np.array([12,18,25,40,56])
-
 plt.grid()
 plt.plot(x, y)
+plt.show()
+
+fig = plt.figure(figsize=(2,2))
+plt.plot(x, y)
+plt.show()
+
+plt.plot(x, y)
+plt.xlim(10,100)
+plt.ylim(100,1000)
 plt.show()
 
 plt.plot(x, y, 'o')
 plt.title("NM Company",loc = 'right')
 plt.xlabel("incentive percentage", fontdict = {'family':'serif','color':'blue','size':20})
 plt.ylabel("sales")
+plt.xticks(rotation=45)
 plt.show()
 
 plt.grid(axis = 'y',color = 'green', linestyle = '--', linewidth = 0.5)
-plt.plot(x)
+plt.plot(x) # plt.clf()
 plt.plot(y)
 plt.show()
+plt.savefig("./chart.png")
 
 plt.subplot(2, 3, 1)
 plt.plot(x)
--- 04_Types_ofPlots.py
+++ 04_Types_ofPlots.py
@@ -1,5 +1,8 @@
 from matplotlib import pyplot as plt
 import numpy as np
+# sudo pip install -U matplotlib to install matplotlib-3.3.4 
+# apt-get remove -y python3-matplotlib to remove existing version. Then 3D will work
+from mpl_toolkits import mplot3d
 
 x = np.array([12,18,25,40,56])
 y = np.array([78,123,389,400,576])
@@ -24,6 +27,9 @@ plt.show()
 plt.scatter(x, y, s=np.array([100,200,500,1000,60]), alpha=0.5)
 plt.show()
 
+plt.axes(projection='3d').scatter3D(x,y) # plot3D
+plt.show()
+
 plt.bar(x,y)
 plt.show()
 
@@ -45,3 +51,4 @@ plt.show()
 plt.pie(x, labels=["E","D","C","B","A"], explode = [0.3, 0.1, 0, 0, 0], shadow = True)
 plt.legend(title = "Employee Ratings")
 plt.show() 
+
